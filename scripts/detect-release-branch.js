ac1b6a1dde28db43f5c0b68dc16ec969eccdec1f
Kevin Frejdh <keffler97@live.se>
2023-03-05 11:17:49 +0100

    Init

---
+++ scripts/detect-release-branch.js
@@ -0,0 +1,28 @@
+// https://github.com/actions/github-script
+
+module.exports = ({github, context, core}) => {
+    const {
+        projectFramework,
+        mavenBranch,
+        gradleBranch,
+        npmBranch,
+        pypiBranch
+    } = process.env;
+
+    let releaseBranch = 'releases';
+    switch (projectFramework.toLowerCase()) {
+        case 'maven':
+            releaseBranch = mavenBranch || 'mvn';
+            break;
+        case 'gradle':
+            releaseBranch = gradleBranch || 'mvn';
+            break;
+        case 'npm':
+            releaseBranch = npmBranch || 'npm'
+            break;
+        case 'python':
+            releaseBranch = pypiBranch || 'pypi'
+            break;
+    }
+    core.exportVariable('RELEASE_BRANCH', releaseBranch)
+}
