7f4acdcc7a18e65ed40c613b85f2ebb7b7bcbc61
Kevin Frejdh <keffler97@live.se>
2023-03-21 17:39:17 +0100

    Added code to checkout different branches in the procedure

--- src/find-and-copy-artifacts.js
+++ src/find-and-copy-artifacts.js
@@ -3,11 +3,14 @@ import {GradleArtifact} from "./artifact/gradle-artifact.js";
 import {NpmArtifact} from "./artifact/npm-artifact.js";
 import {PyPiArtifact} from "./artifact/pypi-artifact.js";
 import * as core from "@actions/core";
+import {execAndGetOutput} from "./util/cmd";
 
 // https://github.com/actions/github-script
 export default async function script() {
     const {
-        projectFramework
+        projectFramework,
+        defaultBranch,
+        releaseBranch
     } = process.env;
 
     let frameworkImpl = undefined;
@@ -29,11 +32,14 @@ export default async function script() {
             throw new Error('Project framework not known')
     }
 
+    await execAndGetOutput('git', ['checkout', defaultBranch]);
     const filesToInspect = await frameworkImpl.getFilesToInspect();
     const artifactsToCopy = await frameworkImpl.getArtifactsToCopy(filesToInspect);
     if (!artifactsToCopy?.length) {
         throw new Error('No artifacts found');
     }
+
+    await execAndGetOutput('git', ['checkout', releaseBranch]);
     await frameworkImpl.copyArtifacts(artifactsToCopy);
 
     core.exportVariable('RELEASE_VERSION', artifactsToCopy[0].version);
